d7a411d86b37d24e37d1a580053805ab6b00892f
Rey-sutare <145734181+Rey-sutare@users.noreply.github.com>
2023-09-22 10:33:57 +0800

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,8 @@
+name = input('Enter your Name:')
+age = int(input('Enter your Name:'))
+address=input('Enter your Address:')
+
+print("Information Received")
+print(f"Name:{name}")
+print(f"Age:{age}")
+print(f"Address:{address}")
