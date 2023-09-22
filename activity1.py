c00df920ed1d7705136a65b92f979dd591b05745
rey <145734181+Rey-sutare@users.noreply.github.com>
2023-09-22 11:56:53 +0800

    Delete activity1.py

--- activity1.py
+++
@@ -1,8 +0,0 @@
-name = input('Enter your Name:')
-age = int(input('Enter your Name:'))
-address=input('Enter your Address:')
-
-print("Information Received")
-print(f"Name:{name}")
-print(f"Age:{age}")
-print(f"Address:{address}")
